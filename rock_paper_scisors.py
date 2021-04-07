6010fc5c04b1e0ca34675afb7f1a6ea009e61adf
adaflos <jimapostolo@gmail.com>
2021-04-08 02:21:16 +0300

    Fixed bug for different values except 1,2,3.

--- rock_paper_scisors.py
+++ rock_paper_scisors.py
@@ -5,17 +5,23 @@ print("Welcome To Rock, Paper, Scissors!")
 print("----------")
 
 answer="y"
+a=1
 
 while answer=="Y" or answer=="y":
     print("Select one of three!")
     print("----------")
-
     print("1. Rock\n2. Paper\n3. Scissors")
     print("----------")
 
     a=int(input())
-    print("----------")
+    if a!=1 and a!=2 and a!=3:
+        print("\n\n")
+        print("----------")
+        print("Wrong input. Please select an option between 1 and 3")
+        print("----------\n\n\n")
+        continue
 
+    print("----------")
     print("Choosing...")
     print("----------")
 
@@ -26,10 +32,10 @@ while answer=="Y" or answer=="y":
             print("Rock!\n\n")
             print("It's a Tie!")
         elif b==3:
-            print("Paper!\n\n")
+            print("Scissors!\n\n")
             print("You Won!")
         elif b==2:
-            print("Scissors!\n\n")
+            print("Paper!\n\n")
             print("You Lost!")
     elif a==2:
         if b==2:
